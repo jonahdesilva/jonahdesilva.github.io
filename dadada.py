f9fa690b3d44a795846955157b638d797ea65166
jonahdesilva <jonahdesilva@icloud.com>
2026-07-01 21:26:20 +0100

    added about me and changed colours

--- dadada.py
+++
@@ -1,9 +0,0 @@
-import os
-
-with open("dada.txt", "a") as f:
-    for file in os.listdir("media/photos"):
-        f.write(f"""
-        <figure class="gallery-item">
-            <img src="media/photos/{file}">
-        </figure>
-        """)
